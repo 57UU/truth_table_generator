c2818d7a83472628c3e1f369db595add5faa76d3
57U <2310827272@qq.com>
2024-09-17 20:26:34 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,155 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button,Input,Card,Typography, Alert,message, Space,Row,Col }  from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import GetLatex from './GetLatex';
+import "./App.css"
 
+
+const horizon={ display: 'flex', justifyContent: 'space-between', width: '100%' }
+const { Search } = Input;
 function App() {
+  const [expression,setExpresion]=useState("")
+  const setDefault = (value, _e, info) => setExpresion("(P&Q)|R");
+  const handleChange = (e) => {
+    setExpresion(e.target.value.toUpperCase());
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{width:"100%"}}>
+      <Space direction="vertical" style={{width:"100%"}}> 
+      <div style={horizon}>
+        {getManual()}
+        {getBanner("真值表计算器")}
+      </div>
+      
+      <Alert 
+        message="注意：在使用+或*运算符时，他们的优先级是相同的"
+        type="info"
+        showIcon
+      />
+      </Space>
+
+      
+      <div style={{padding:"1rem 1rem 1rem 1rem"}}>
+      <Search
+        style={{
+          width: "100%",
+        }}
+        value={expression}
+        onChange={handleChange}
+        addonBefore="键入数学表达式"
+        placeholder="input expression"
+        allowClear
+        onSearch={setDefault}
+        enterButton="Try One"
+        size="large"
+      />
+      {generateTable(handleExpression(expression))}
+      </div>
     </div>
   );
+}
+function generateTable(c){
+  let tableLatex;
+  let content;
+  if(c==undefined) {
+    tableLatex="请输入或输入有误"
+    content="请输入或输入有误"
+  }else{
+    content=c.content
+    let contentList=content.split("\n")
+tableLatex=String.raw`
+\begin{table}[H]
+    \centering
+    \begin{tabular}{${"|c".repeat(c.columns)}|}
+        \toprule[1.5pt]
+        ${contentList[0]}
+        \hline
+${contentList.slice(1).map(i=>`        ${i}`).join("\n")}
+        \bottomrule[1.5pt]
+    \end{tabular}
+\end{table}
+`.trimStart()
+  }
+  return(<>
+  {getLatexBlock(tableLatex,"带有表格的LaTeX Code")}
+  </>
+  )
+}
+let lastExpression=undefined;
+let lastValue=undefined;
+function handleExpression(str) {
+    if(lastExpression==str){
+      return lastValue;
+    }
+    try{
+      lastValue= GetLatex(str)
+      lastExpression=str
+      return lastValue;
+    }catch(err){
+      return undefined;
+    }
+}
+function getLatexBlock(c,title="LaTeX Code"){
+  const copy=()=>{
+    navigator.clipboard.writeText(c).then(() => {
+      // 成功复制后，可以在这里添加一些反馈，比如弹窗或者状态消息
+      message.success('代码已复制到剪贴板！');
+    }).catch(err => {
+      // 如果复制失败，可以在这里处理错误
+      message.error('复制失败:', err);
+    });
+  }
+  return(<div style={{padding:"1rem 0 0 0"}}>
+    <div style={horizon}>
+      <p>{title}</p>
+      <Button type='primary' onClick={copy}>Copy</Button>
+    </div>
+    <pre>
+    <code>
+        {c}
+      </code>
+    </pre>
+    </div>)
+}
+const manual=[
+  '非：~ 或 !',
+  "或：| 或 +",
+  '与：& 或 *',
+  '蕴含：> ',
+  '等价：='
+]
+
+
+function getManual(){
+  return (
+    <Card title="说明" bordered={true}
+     style={{ 
+      width: 300 ,
+      backgroundColor: 'green', // Ant Design的蓝色调
+      fontSize: '1rem', 
+      }}>
+    <p>{manual.map(e=><div>{e}</div>)}</p>
+  </Card>
+  )
+}
+function getBanner(text){
+  return ( <Card
+  style={{
+    width:"100%",
+    textAlign: 'center',
+    backgroundColor: '#1890ff', // Ant Design的蓝色调
+    color: '#fff', // 白色文字
+    fontSize: '2rem', // 你可以根据需要调整字体大小
+    fontWeight: 'bold', // 加粗文字
+    padding: '6rem 0', // 内边距调整
+    margin:"0 0 0 0.5rem"
+  }}
+  bodyStyle={{ padding: 0 }} // 移除Card内部padding
+>
+  {text}
+</Card>)
 }
 
 export default App;
---
+++ src/GetLatex.js
@@ -0,0 +1,264 @@
+function GetLatex(str){
+    str=str.replace("!","~").replace("+","|").replace("*","&").replace("！","~").replace("（","(").replace("）",")")
+    str=[...str]
+    verify(str)
+    const varibles=getVaribles(str)
+    if(varibles.length==0){
+      return undefined
+    }
+    const postfix=infix2postfix(str)
+    console.log("POSTFIX:"+postfix)
+    const astRoot=postfix2ast(postfix)
+    const varibles_num=varibles.length
+    let contexts=[]
+    let recorder=[]
+    for(let i=0;i<2**varibles_num;i++){
+      let values=[...parseInt(i).toString(2)]
+      var hashmap = new Map();
+      for(let j=0;j<varibles_num;j++){
+        hashmap[varibles[j]]=values[j]=='1'
+      }
+      contexts.push(hashmap)
+    }
+    const truthTable=astRoot.getTruthTable(contexts,0,recorder)
+    recorder.sort(function(a,b){return b.layer-a.layer})
+
+    let rows=[]
+    let top=""
+    let _a=varibles.map(c=>`\$${c}\$`)
+    let _b=recorder.map(ele=>`\$${convert2latex(ele.infix)}\$`)
+    let all=_a.concat(_b)
+    top+=all.join(" & ")
+    rows.push(top)
+    for(let i=0;i<contexts.length;i++){
+      let row=""
+      let a1=varibles.map(v=>`${contexts[i][v]?1:0}`)
+      let a2=recorder.map(ele=>`${ele.table[i]?1:0}`)
+      let all=a1.concat(a2)
+      row+=all.join(" & ")
+      rows.push(row)
+    }
+
+    const content= rows.join("\\\\ \n")+"\\\\"
+    return {
+      content:content,
+      columns:varibles_num+recorder.length
+    }
+}
+function convert2latex(str){
+  str=str.replace("&"," \\land ").replace("|"," \\lor ").replace(">"," \\rightarrow ").replace("="," \\leftrightarrow ").replace("~"," \\sim ")
+  return str
+}
+function verify(list){
+  let stack=new Stack()
+  for(const i of list){
+    if(i=="("){
+      stack.push(i)
+    }else if(i==")"){
+      if(stack.isEmpty()){
+        throw new SyntaxError()
+      }
+      stack.pop()
+    }
+  }
+  if(stack.isEmpty()){
+    //ok
+  }else{
+    throw new SyntaxError()
+  }
+}
+function getVaribles(list){
+  let set=new Set();
+  for(let j=0;j<list.length;j++){
+    let i=list[j]
+    if("A"<=i && i<="Z"){
+      set.add(i)
+    }
+  }
+
+  
+  return [...set]
+}
+const prec={
+  "(":1,
+  ")":1,
+  "=":1.5,
+  ">":1.5,
+  "|":2,
+  "&":2,
+  "~":4,
+  "$":5
+}
+const operations={
+  equal:"=",
+  or:"|",
+  and:"&",
+  not:"~",
+  if:">",
+  immediate:"$"
+};
+function infix2postfix(exp){
+    let op_stack =new Stack();
+    let postfix=[];
+    exp=[...exp]
+    for (const element of exp) {
+      if('A'<=element && element<='Z'){
+        postfix.push(element)
+      }else if(element=="("){
+        op_stack.push(element)
+      }else if(element==")"){
+        let top=op_stack.pop()
+        while(top!="("){
+          postfix.push(top);
+          top=op_stack.pop()
+        }
+      }else{
+        while(!op_stack.isEmpty()&&prec[op_stack.peek()>=prec[element]]){
+          postfix.push(op_stack.pop());
+        }
+        op_stack.push(element)
+      }
+    }
+    while(!op_stack.isEmpty()){
+      postfix.push(op_stack.pop());
+    }
+    return postfix
+
+}
+function postfix2ast(exp){
+  const stack=new Stack()
+  for (const element of exp){
+    if('A'<=element && element<='Z'){
+      stack.push(new AstNode(operations.immediate,element))
+    }else if(element==operations.not){
+      const ele=stack.pop()
+      const ele_new=new AstNode(operations.not,ele)
+      stack.push(ele_new)
+    }else{
+      const ele2=stack.pop();
+      const ele1=stack.pop();
+      const ele_new=new AstNode(element,ele1,ele2)
+      stack.push(ele_new)
+    }
+  }
+  return stack.pop()
+}
+class AstNode{
+  constructor(op,ele1,ele2){
+    this.op=op;
+    this.ele1=ele1;
+    this.ele2=ele2;
+    this.infix=undefined;
+  }
+  getInfix(){
+    if(this.infix!=undefined){
+      return this.infix
+    }
+    if(this.op=="$"){//立即数
+      this.infix=this.ele1
+    }else{
+      if(this.ele2==undefined){//单操作符
+        if(prec[this.op]>prec[this.ele1.op]){
+          this.infix=`${this.op}(${this.ele1.getInfix()})`
+        }else{
+          this.infix=`${this.op}${this.ele1.getInfix()}`
+        }
+      }else{//双操作符
+        const left=prec[this.op]>prec[this.ele1.op]?`(${this.ele1.getInfix()})`:this.ele1.getInfix()
+        const right=prec[this.op]>prec[this.ele2.op]?`(${this.ele2.getInfix()})`:this.ele2.getInfix()
+        this.infix=left+this.op+right
+      }
+    }
+    return this.infix
+  }
+  getTruthTable(contexts,layer,recorder){
+    if(this.op==operations.immediate){
+      return contexts.map(e=>e[this.ele1])
+    }
+    const ele1_table=this.ele1.getTruthTable(contexts,layer+1,recorder)
+    if(this.op==operations.not){ 
+      let table=ele1_table.map(v=>!v)
+      if(layer==0){
+        recorder.push({
+          layer:layer,
+          infix:this.getInfix(),
+          table:table
+        })
+      }
+      return table
+    }
+    const ele2_table=this.ele2.getTruthTable(contexts,layer+1,recorder)
+    let table=[]
+    if(this.op==operations.and){
+      for(let i=0;i<ele1_table.length;i++){
+        table.push(ele1_table[i]&&ele2_table[i])
+      }
+    }else if(this.op==operations.or){
+      for(let i=0;i<ele1_table.length;i++){
+        table.push(ele1_table[i]||ele2_table[i])
+      }
+    }else if(this.op==operations.if){
+      for(let i=0;i<ele1_table.length;i++){
+        table.push(!ele1_table[i]||ele2_table[i])
+      }
+    }else if(this.op==operations.equal){
+      for(let i=0;i<ele1_table.length;i++){
+        table.push(ele1_table[i]==ele2_table[i])
+      }
+    }
+    recorder.push({
+      layer:layer,
+      infix:this.getInfix(),
+      table:table
+    })
+    return table
+  }
+}
+
+class Stack {
+  constructor() {
+    this.items = []; // 使用数组存储栈内元素
+  }
+
+  // 入栈操作
+  push(element) {
+    this.items.push(element);
+  }
+
+  // 出栈操作
+  pop() {
+    if (this.isEmpty()) {
+      return undefined; // 如果栈为空，则返回undefined
+    }
+    return this.items.pop();
+  }
+
+  // 查看栈顶元素
+  peek() {
+    if (this.isEmpty()) {
+      return undefined; // 如果栈为空，则返回undefined
+    }
+    return this.items[this.items.length - 1];
+  }
+
+  // 检查栈是否为空
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  // 清空栈
+  clear() {
+    this.items = [];
+  }
+
+  // 返回栈的大小
+  size() {
+    return this.items.length;
+  }
+
+  // 打印栈内元素
+  print() {
+    console.log(this.items.toString());
+  }
+}
+export default GetLatex;
